ca1ad366330f804e0139bc967dc4837d3f0e9fd3
puttyreddyreshma <37476536+puttyreddyreshma@users.noreply.github.com>
2018-03-17 20:17:05 +0530

    Add files via upload

---
+++ WindowsFormsApplication5/ClassLibrary12/ClassLibrary12/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace ClassLibrary12
+{
+    [Serializable]
+    public class Class1
+    {
+        int i, j;
+        public void set(int s, int r)
+        {
+            i = s;
+            j = r;
+        }
+        public int sum()
+        {
+            return i + j;
+        }
+        public int product()
+        {
+            return i * j;
+        }
+
+    }
+}
---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary12;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 c = new Class1();
+            int s1 = int.Parse(textBox1.Text);
+            int s2 = int.Parse(textBox2.Text);
+            c.set(s1, s2);
+            FileStream fs = File.Create(@"G:\\arith.dat");
+            BinaryFormatter b = new BinaryFormatter();
+            b.Serialize(fs, c);
+            fs.Close();
+            MessageBox.Show("serialized");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = File.OpenRead(@"G:\\arith.dat");
+            BinaryFormatter b = new BinaryFormatter();
+            Class1 c = (Class1)b.Deserialize(fs);
+            int a = c.sum();
+            int d = c.product();
+            textBox3.Text = a.ToString();
+            textBox4.Text = d.ToString();
+            fs.Close();
+            MessageBox.Show("deserialized");
+
+
+        }
+    }
+}
